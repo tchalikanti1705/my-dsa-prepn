ee6c37fb12e6c2ecf9fd237222416114b2d80f3c
Teja Chalikanti <tchalikanti@gmail.com>
2026-02-01 21:38:50 -0500

    Remove duplicates from sorted array using two-pointer
    Implemented two-pointer approach to remove duplicates from sorted array.

--- Arrays/Basic Operations/Remove_Duplicates_From_Sorted_Array_1.py
+++ Arrays/Basic Operations/Remove_Duplicates_From_Sorted_Array_1.py
@@ -7,9 +7,6 @@ class Solution:
                 left+=1
         return left
 
-
-
-
 '''
 I solved “Remove Duplicates from Sorted Array” using the two-pointer overwrite approach. Since the array is already sorted, all duplicates come next to each other. I keep a pointer left that represents the position where the next unique number should be placed, starting from index 1 (because the first element is always unique). Then I move right from index 1 to the end and compare nums[right] with nums[right-1]. If they are different, that means nums[right] is a new unique value, so I copy it into nums[left] and move left forward. By the end, the first left elements of the array are the unique values in order, and I return left as the count of unique elements.
 
