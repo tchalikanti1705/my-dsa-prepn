9ab29a7275963c12ae710fbafb10252920f16d15
Teja Chalikanti <tchalikanti@gmail.com>
2026-03-10 22:21:45 -0400

    Create dfs.py

---
+++ Arrays/Basic Operations/dfs.py
@@ -0,0 +1,47 @@
+# DFS using recursion
+
+def dfs(graph, node, visited):
+    visited.add(node)
+    print(node, end=" ")
+
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+
+# Graph represented as adjacency list
+graph = {
+    0: [1, 2],
+    1: [0, 3, 4],
+    2: [0],
+    3: [1],
+    4: [1]
+}
+
+visited = set()
+
+print("DFS Traversal starting from node 0:")
+dfs(graph, 0, visited)
+
+
+def dfs_iterative(graph, start):
+    visited = set()
+    stack = [start]
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            stack.extend(reversed(graph[node]))
+
+graph = {
+    0: [1, 2],
+    1: [0, 3, 4],
+    2: [0],
+    3: [1],
+    4: [1]
+}
+
+print("DFS Traversal:")
+dfs_iterative(graph, 0)
